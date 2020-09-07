407bfeee4c43e45f91dad5e9fc01b444363da8e1
Michal Swietek <michael.swietek@alephzero.org>
2020-09-07 22:10:25 +0200

    alice-bob gossip

---
+++ bin/ab-node/node/src/service.rs
@@ -0,0 +1,215 @@
+//! Service and ServiceFactory implementation. Specialized wrapper over substrate service.
+
+use std::sync::Arc;
+use std::pin::Pin;
+use sc_client_api::{ExecutorProvider, RemoteBackend};
+use node_template_runtime::{self, opaque::Block, RuntimeApi};
+use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
+use sp_inherents::InherentDataProviders;
+use sc_executor::native_executor_instance;
+pub use sc_executor::NativeExecutor;
+use sp_consensus_aura::sr25519::{AuthorityPair as AuraPair};
+use sc_finality_grandpa::{FinalityProofProvider as GrandpaFinalityProofProvider};
+use log::info;
+use sp_core::crypto::key_types::DUMMY;
+use parking_lot::Mutex;
+use futures_sink::Sink;
+use futures::stream::StreamExt;
+
+use ab_gossip::{NetworkBridge, LocalIdKeystore, Message, Error::*};
+
+
+// Our native executor instance.
+native_executor_instance!(
+	pub Executor,
+	node_template_runtime::api::dispatch,
+	node_template_runtime::native_version,
+);
+
+type FullClient = sc_service::TFullClient<Block, RuntimeApi, Executor>;
+type FullBackend = sc_service::TFullBackend<Block>;
+type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
+
+pub fn new_partial(config: &Configuration) -> Result<sc_service::PartialComponents<
+	FullClient, FullBackend, FullSelectChain,
+	sp_consensus::DefaultImportQueue<Block, FullClient>,
+	sc_transaction_pool::FullPool<Block, FullClient>,
+	(
+		sc_finality_grandpa::GrandpaBlockImport<FullBackend, Block, FullClient, FullSelectChain>,
+		sc_finality_grandpa::LinkHalf<Block, FullClient, FullSelectChain>
+	)
+>, ServiceError> {
+	let inherent_data_providers = sp_inherents::InherentDataProviders::new();
+
+	let (client, backend, keystore, task_manager) =
+		sc_service::new_full_parts::<Block, RuntimeApi, Executor>(&config)?;
+	let client = Arc::new(client);
+
+	let select_chain = sc_consensus::LongestChain::new(backend.clone());
+
+	let transaction_pool = sc_transaction_pool::BasicPool::new_full(
+		config.transaction_pool.clone(),
+		config.prometheus_registry(),
+		task_manager.spawn_handle(),
+		client.clone(),
+	);
+
+	let (grandpa_block_import, grandpa_link) = sc_finality_grandpa::block_import(
+		client.clone(), &(client.clone() as Arc<_>), select_chain.clone(),
+	)?;
+
+	let aura_block_import = sc_consensus_aura::AuraBlockImport::<_, _, _, AuraPair>::new(
+		grandpa_block_import.clone(), client.clone(),
+	);
+
+	let import_queue = sc_consensus_aura::import_queue::<_, _, _, AuraPair, _, _>(
+		sc_consensus_aura::slot_duration(&*client)?,
+		aura_block_import,
+		Some(Box::new(grandpa_block_import.clone())),
+		None,
+		client.clone(),
+		inherent_data_providers.clone(),
+		&task_manager.spawn_handle(),
+		config.prometheus_registry(),
+		sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone()),
+	)?;
+
+	Ok(sc_service::PartialComponents {
+		client, backend, task_manager, import_queue, keystore, select_chain, transaction_pool,
+		inherent_data_providers,
+		other: (grandpa_block_import, grandpa_link),
+	})
+}
+
+/// Builds a new service for a full client.
+pub fn new_full(config: Configuration) -> Result<TaskManager, ServiceError> {
+	let sc_service::PartialComponents {
+		client, task_manager, import_queue, keystore, transaction_pool, ..
+	} = new_partial(&config)?;
+
+        let keys = (keystore.clone() as sp_core::traits::BareCryptoStorePtr).read().ed25519_public_keys(DUMMY);
+        let public = if keys.len() > 0 {
+            keys[0]
+        } else {
+            (keystore.clone() as sp_core::traits::BareCryptoStorePtr).write().ed25519_generate_new(DUMMY, None).unwrap()
+        };
+
+        let name = config.network.node_name.clone();
+        info!("{} key is {}", name, public);
+
+	let (network, _, _, network_starter) =
+		sc_service::build_network(sc_service::BuildNetworkParams {
+			config: &config,
+			client: client.clone(),
+			transaction_pool: transaction_pool.clone(),
+			spawn_handle: task_manager.spawn_handle(),
+			import_queue,
+			on_demand: None,
+			block_announce_validator_builder: None,
+			finality_proof_request_builder: None,
+			finality_proof_provider: None,
+		})?;
+
+        let nb = NetworkBridge::new(network);
+        let keystore = LocalIdKeystore::from((public.into(), keystore.clone()  as sp_core::traits::BareCryptoStorePtr));
+        let (mut incoming, outgoing) = nb.round_communication(0, Some(keystore));
+
+        task_manager.spawn_handle().spawn("network bridge", nb);
+
+        let data = name.clone();
+        task_manager.spawn_handle().spawn("receive", async move {
+            if let Some(signed) = incoming.next().await {
+                info!("{} received message {:?}", data, signed.message.data);
+            } 
+        });
+ 
+
+	let outgoing = Arc::new(Mutex::new(outgoing));
+        let data = name.clone();
+        task_manager.spawn_handle().spawn("send", async move {
+            match Pin::new(&mut *outgoing.lock()).start_send(Message{data: data.clone()}) {
+                Ok(()) => info!("{} sent message", data),
+                Err(Signing(err)) | Err(Network(err)) => info!("error while sending {}", err),
+            }
+        });
+
+
+	network_starter.start_network();
+	Ok(task_manager)
+}
+
+/// Builds a new service for a light client.
+pub fn new_light(config: Configuration) -> Result<TaskManager, ServiceError> {
+	let (client, backend, keystore, mut task_manager, on_demand) =
+		sc_service::new_light_parts::<Block, RuntimeApi, Executor>(&config)?;
+
+	let transaction_pool = Arc::new(sc_transaction_pool::BasicPool::new_light(
+		config.transaction_pool.clone(),
+		config.prometheus_registry(),
+		task_manager.spawn_handle(),
+		client.clone(),
+		on_demand.clone(),
+	));
+
+	let grandpa_block_import = sc_finality_grandpa::light_block_import(
+		client.clone(), backend.clone(), &(client.clone() as Arc<_>),
+		Arc::new(on_demand.checker().clone()) as Arc<_>,
+	)?;
+	let finality_proof_import = grandpa_block_import.clone();
+	let finality_proof_request_builder =
+		finality_proof_import.create_finality_proof_request_builder();
+
+	let import_queue = sc_consensus_aura::import_queue::<_, _, _, AuraPair, _, _>(
+		sc_consensus_aura::slot_duration(&*client)?,
+		grandpa_block_import,
+		None,
+		Some(Box::new(finality_proof_import)),
+		client.clone(),
+		InherentDataProviders::new(),
+		&task_manager.spawn_handle(),
+		config.prometheus_registry(),
+		sp_consensus::NeverCanAuthor,
+	)?;
+
+	let finality_proof_provider =
+		GrandpaFinalityProofProvider::new_for_service(backend.clone(), client.clone());
+
+	let (network, network_status_sinks, system_rpc_tx, network_starter) =
+		sc_service::build_network(sc_service::BuildNetworkParams {
+			config: &config,
+			client: client.clone(),
+			transaction_pool: transaction_pool.clone(),
+			spawn_handle: task_manager.spawn_handle(),
+			import_queue,
+			on_demand: Some(on_demand.clone()),
+			block_announce_validator_builder: None,
+			finality_proof_request_builder: Some(finality_proof_request_builder),
+			finality_proof_provider: Some(finality_proof_provider),
+		})?;
+
+	if config.offchain_worker.enabled {
+		sc_service::build_offchain_workers(
+			&config, backend.clone(), task_manager.spawn_handle(), client.clone(), network.clone(),
+		);
+	}
+
+	sc_service::spawn_tasks(sc_service::SpawnTasksParams {
+		remote_blockchain: Some(backend.remote_blockchain()),
+		transaction_pool,
+		task_manager: &mut task_manager,
+		on_demand: Some(on_demand),
+		rpc_extensions_builder: Box::new(|_, _| ()),
+		telemetry_connection_sinks: sc_service::TelemetryConnectionSinks::default(),
+		config,
+		client,
+		keystore,
+		backend,
+		network,
+		network_status_sinks,
+		system_rpc_tx,
+	 })?;
+
+	 network_starter.start_network();
+
+	 Ok(task_manager)
+}
---
+++ client/ab-gossip/src/lib.rs
@@ -0,0 +1,232 @@
+use codec::{Encode, Decode};
+use log::info;
+
+use sc_network::{NetworkService, PeerId};
+use sc_network_gossip::{GossipEngine, Validator, ValidationResult, ValidatorContext};
+
+use sp_runtime::traits::{Block as BlockT, Hash as HashT, Header as HeaderT};
+use sp_core::traits::BareCryptoStorePtr;
+
+use parking_lot::{Mutex, RwLock};
+use std::{pin::Pin, sync::Arc, task::{Context, Poll}};
+use futures::{prelude::*, channel::mpsc};
+
+const AB_GOSSIP_ID: [u8; 4] = *b"abgo";
+const AB_PROTOCOL_NAME: &[u8] = b"/abgossip";
+
+pub const KEY_TYPE: sp_core::crypto::KeyTypeId = sp_application_crypto::key_types::DUMMY;
+mod app {
+    use sp_application_crypto::{app_crypto, key_types::DUMMY, ed25519};
+    app_crypto!(ed25519, DUMMY);
+}
+
+pub type AuthorityId = app::Public;
+pub type AuthoritySignature = app::Signature;
+
+pub struct LocalIdKeystore((AuthorityId, BareCryptoStorePtr));
+
+impl LocalIdKeystore {
+    fn local_id(&self) -> &AuthorityId {
+        &(self.0).0
+    }
+
+    fn keystore(&self) -> &BareCryptoStorePtr {
+        &(self.0).1
+    }
+}
+
+impl AsRef<BareCryptoStorePtr> for LocalIdKeystore {
+    fn as_ref(&self) -> &BareCryptoStorePtr {
+        self.keystore()
+    }
+}
+
+impl From<(AuthorityId, BareCryptoStorePtr)> for LocalIdKeystore {
+	fn from(inner: (AuthorityId, BareCryptoStorePtr)) -> LocalIdKeystore {
+		LocalIdKeystore(inner)
+	}
+}
+
+#[derive(Debug, Clone, Encode, Decode)]
+pub struct Message{
+    pub data: String
+}
+
+impl Message {
+    pub fn sign(&self, public: AuthorityId, keystore: &BareCryptoStorePtr) -> Option<SignedMessage> {
+        use sp_core::crypto::Public;        // to_public_crypto_pair
+        use sp_application_crypto::AppKey;  // ID
+        use sp_std::convert::TryInto;       // try_into
+
+        let encoded = self.encode();
+        let signature = keystore.read()
+            .sign_with(AuthorityId::ID, &public.to_public_crypto_pair(), &encoded[..])
+            .ok()?.try_into().ok()?;
+
+        Some(SignedMessage{message: self.clone(), signature, id: public})
+    } 
+}
+
+#[derive(Debug, Clone, Encode, Decode)]
+pub struct SignedMessage{
+    pub message: Message,
+    pub signature: AuthoritySignature,
+    pub id: AuthorityId,
+}
+
+#[derive(Debug, Encode, Decode)]
+pub struct GossipMessage {
+    pub round: u8,
+    pub message: SignedMessage,
+}
+
+fn round_topic<B: BlockT>(round: u8) -> B::Hash {
+    <<B::Header as HeaderT>::Hashing as HashT>::hash(round.to_string().as_bytes())
+}
+
+pub struct GossipValidator {
+    round: RwLock<u8>
+}
+
+impl GossipValidator {
+    pub fn new() -> Self {
+        GossipValidator {round: RwLock::new(0)}
+    }
+    
+    pub fn note_round(&self, round: u8){
+        *self.round.write() = round;
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    Network(String),
+    Signing(String),
+}
+
+impl<B: BlockT> Validator<B> for GossipValidator{
+    fn validate(&self, _context: &mut dyn ValidatorContext<B>, _sender: &PeerId, data: &[u8]) -> ValidationResult<B::Hash> {
+        match GossipMessage::decode(&mut data.clone()) {
+            Ok(gm) => {
+                let topic = round_topic::<B>(gm.round);
+                ValidationResult::ProcessAndKeep(topic)
+            }
+            Err(e) => {
+                info!(target: "ab-gossip", "Error decoding message: {}", e.what());
+                ValidationResult::Discard
+            }
+        }
+    }
+}
+
+pub struct OutgoingMessages<B: BlockT> {
+    round: u8,
+    sender: mpsc::Sender<SignedMessage>,
+    keystore: Option<LocalIdKeystore>,
+    network: Arc<Mutex<GossipEngine<B>>>,
+}
+
+impl<B: BlockT> Sink<Message> for OutgoingMessages<B> {
+    type Error = Error;
+
+    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Sink::poll_ready(Pin::new(&mut self.sender), cx).map(|elem| { elem.map_err(|e| {
+            Error::Network(format!("Failed to poll_ready channel sender: {:?}", e))
+        })})
+    }
+
+    fn start_send(mut self: Pin<&mut Self>, msg: Message) -> Result<(), Self::Error> {
+        if let Some(ref keystore) = self.keystore {
+            let signed = msg.sign(keystore.local_id().clone(), keystore.as_ref()).ok_or_else(
+                || Error::Signing(format!("Failed to sign msg {:?}", msg))
+            )?;
+            let message = GossipMessage{round: self.round, message: signed.clone()};
+
+            let topic = round_topic::<B>(self.round);
+            self.network.lock().gossip_message(topic, message.encode(), false);
+
+            return self.sender.start_send(signed).map_err(|e|{
+                Error::Network(format!("Failed to start_send on channel sender: {:?}", e))
+            });
+        }
+
+        Ok(())
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Sink::poll_close(Pin::new(&mut self.sender), cx).map(|elem| { elem.map_err(|e| {
+            Error::Network(format!("Failed to poll_close channel sender: {:?}", e))
+        })})
+    }
+}
+
+pub struct NetworkBridge<B: BlockT> {
+    gossip_engine: Arc<Mutex<GossipEngine<B>>>,
+    validator: Arc<GossipValidator>,
+}
+
+impl<B: BlockT> Unpin for NetworkBridge<B> {}
+
+impl<B: BlockT> NetworkBridge<B> {
+    pub fn new(network: Arc<NetworkService<B, <B as BlockT>::Hash>>) -> Self {
+        let validator = Arc::new(GossipValidator::new());
+        let gossip_engine = Arc::new(Mutex::new(GossipEngine::new(network.clone(), AB_GOSSIP_ID, AB_PROTOCOL_NAME, validator.clone())));
+
+        NetworkBridge{gossip_engine, validator}
+    }
+
+    pub fn note_round(&self, round: u8) {
+        self.validator.note_round(round);
+    }
+
+    pub fn round_communication(&self, round: u8, keystore: Option<LocalIdKeystore>) -> (impl Stream<Item=SignedMessage> + Unpin, OutgoingMessages<B>) {
+        self.note_round(round);
+
+        let topic = round_topic::<B>(round);
+        let incoming = self.gossip_engine.lock().messages_for(topic).
+            filter_map(move |notification| {
+                let decoded = GossipMessage::decode(&mut &notification.message[..]);
+                match decoded {
+                    Ok(gm) => {
+                        info!(target: "ab-gossip", "received messege {:?}", gm.message);
+                        future::ready(Some(gm.message))
+                    }
+                    Err(ref e) => {
+                        info!(target: "ab-gossip", "Skipping malformed message {:?}: {}", notification, e);
+                        future::ready(None)
+                    }
+                }
+            });
+
+        let (tx, _rx) = mpsc::channel(0);
+        let outgoing = OutgoingMessages::<B> {
+            round,
+            sender: tx,
+            keystore,
+            network: self.gossip_engine.clone(),
+        };
+
+        //let incoming = stream::select(incoming, rx);
+
+        (incoming, outgoing)
+    }
+}
+
+impl<B: BlockT> Future for NetworkBridge<B> {
+	type Output = ();
+
+	fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+		match self.gossip_engine.lock().poll_unpin(cx) {
+			Poll::Ready(()) => return Poll::Ready(
+				info!("Gossip engine future finished.")
+			),
+			Poll::Pending => {},
+		}
+
+		Poll::Pending
+	}
+}
