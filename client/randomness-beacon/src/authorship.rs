51a097e8d7f406a0d8905f7104c2fa3b0028961b
Michal Swietek <michal.swietek@cardinals.cc>
2020-10-01 15:50:08 +0200

    discard block proposition if inherent data does not contain random bytes

--- client/randomness-beacon/src/authorship.rs
+++ client/randomness-beacon/src/authorship.rs
@@ -1,14 +1,17 @@
 use codec::Encode;
-use futures::future;
 use log::info;
 use parking_lot::Mutex;
-use sc_block_builder::{BlockBuilderApi, BlockBuilderProvider};
 use sc_client_api::backend;
 use sp_api::{ApiExt, ProvideRuntimeApi};
 use sp_blockchain::HeaderBackend;
-use sp_runtime::traits::{Block as BlockT, Header as HeaderT};
 use sp_transaction_pool::TransactionPool;
 use std::{collections::HashSet, sync::mpsc::Receiver, sync::Arc, time};
+
+use futures::future;
+use sc_block_builder::{BlockBuilderApi, BlockBuilderProvider};
+use sp_consensus::{Proposal, RecordProof};
+use sp_inherents::InherentData;
+use sp_runtime::traits::{Block as BlockT, DigestFor, Header as HeaderT};
 
 use prometheus_endpoint::Registry as PrometheusRegistry;
 
@@ -16,7 +19,7 @@ use super::Nonce;
 
 /// Proposer factory.
 pub struct ProposerFactory<A, B, C> {
-	/// Inner proposer
+	/// Inner propeser
 	inner: sc_basic_authorship::ProposerFactory<A, B, C>,
 	/// Receiver of notifications
 	randomness_notifier: Arc<Mutex<Receiver<Nonce>>>,
@@ -54,11 +57,12 @@ where
 		+ BlockBuilderApi<Block, Error = sp_blockchain::Error>,
 {
 	type CreateProposer = future::Ready<Result<Self::Proposer, Self::Error>>;
-	type Proposer = sc_basic_authorship::Proposer<B, Block, C, A>;
+	type Proposer = Proposer<B, Block, C, A>;
 	type Error = sp_blockchain::Error;
 
 	fn init(&mut self, parent_header: &<Block as BlockT>::Header) -> Self::CreateProposer {
 		let parent_number = *parent_header.number();
+		let mut proposer_nonce = None;
 		// TODO should use global constant
 		if parent_number > 1.into() {
 			let parent_hash = parent_header.hash();
@@ -78,6 +82,7 @@ where
 						"duplicated available_random_bytes notification"
 					);
 					if nonce == new_nonce {
+						proposer_nonce = Some(nonce);
 						info!(
 							target: "ProposerFactory",
 							"got random bytes for hash {:?} of block number {:?}",
@@ -89,8 +94,64 @@ where
 				}
 			}
 		}
-		future::ready(Ok(self
-			.inner
-			.init_with_now(parent_header, Box::new(time::Instant::now))))
+		future::ready(Ok(Proposer {
+			inner: self
+				.inner
+				.init_with_now(parent_header, Box::new(time::Instant::now)),
+			nonce: proposer_nonce,
+		}))
+	}
+}
+
+pub struct Proposer<B, Block: BlockT, C, A: TransactionPool> {
+	inner: sc_basic_authorship::Proposer<B, Block, C, A>,
+	nonce: Option<Nonce>,
+}
+
+impl<A, B, Block, C> sp_consensus::Proposer<Block> for Proposer<B, Block, C, A>
+where
+	A: TransactionPool<Block = Block> + 'static,
+	B: backend::Backend<Block> + Send + Sync + 'static,
+	Block: BlockT,
+	C: BlockBuilderProvider<B, Block, C>
+		+ HeaderBackend<Block>
+		+ ProvideRuntimeApi<Block>
+		+ Send
+		+ Sync
+		+ 'static,
+	C::Api: ApiExt<Block, StateBackend = backend::StateBackendFor<B, Block>>
+		+ BlockBuilderApi<Block, Error = sp_blockchain::Error>,
+{
+	type Transaction = backend::TransactionFor<B, Block>;
+	type Proposal =
+		tokio_executor::blocking::Blocking<Result<Proposal<Block, Self::Transaction>, Self::Error>>;
+	type Error = sp_blockchain::Error;
+
+	fn propose(
+		self,
+		inherent_data: InherentData,
+		inherent_digests: DigestFor<Block>,
+		max_duration: time::Duration,
+		record_proof: RecordProof,
+	) -> Self::Proposal {
+		if let Some(nonce) = self.nonce {
+			let list_nonce_random_bytes: Option<Vec<(Vec<u8>, Vec<u8>)>> = inherent_data
+				.get_data(&sp_randomness_beacon::inherents::INHERENT_IDENTIFIER)
+				.unwrap_or_default();
+			// safe as !self.nonce.is_none implies that we put sth into inherent_data
+			let mut not_found = true;
+			for (n, _) in list_nonce_random_bytes.unwrap() {
+				if n == nonce {
+					not_found = false;
+				}
+			}
+			if not_found {
+				return tokio_executor::blocking::run(|| {
+					Err(sp_blockchain::Error::Msg("no inherent data".to_string()))
+				});
+			}
+		}
+		self.inner
+			.propose(inherent_data, inherent_digests, max_duration, record_proof)
 	}
 }
