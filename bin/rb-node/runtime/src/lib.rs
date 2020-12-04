1de530831e551602918fb90d0849255f80666f80
Michal Swietek <michal.swietek@cardinals.cc>
2020-12-04 13:27:31 +0100

    add dkg benchmark to runtime

--- bin/rb-node/runtime/src/lib.rs
+++ bin/rb-node/runtime/src/lib.rs
@@ -616,6 +616,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, pallet_balances, Balances);
 			add_benchmark!(params, batches, pallet_timestamp, Timestamp);
 			add_benchmark!(params, batches, pallet_randomness_beacon, RandomnessBeacon);
+			add_benchmark!(params, batches, pallet_dkg, DKG);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
